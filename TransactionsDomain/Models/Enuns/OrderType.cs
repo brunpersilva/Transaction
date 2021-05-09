8f318232af587100e1925accc80ee2b44daee866
Bruno Silva <brunpersilva@gmail.com>
2021-05-09 13:09:00 -0300

    Created project and added domain library

---
+++ TransactionsDomain/Models/Enuns/OrderType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransactionsDomain.Models.Enuns
+{
+    public enum OrderType
+    {
+        Purshace = 1,
+        Sellout = 2
+    }
+}
---
+++ TransactionsDomain/Models/Enuns/TransactionStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransactionsDomain.Models.Enuns
+{
+    public enum TransactionStatus
+    {
+        Open = 1,
+        Executed = 2,
+        Rejectd = 3,
+    }
+}
